ac40b870283f0971300160c5d5e6b3060626206e
Philip Hazelden <philip.hazelden@gmail.com>
2010-08-21 10:44:11 +0100

    Window is black and closes.

--- src/gw.cpp
+++ src/gw.cpp
@@ -1,7 +1,28 @@
 #include "globals.h"
 #include "includes.h"
 
+void handleEvents();
+void handleEvent(sf::Event e);
+
 int main (int argc, char **argv) {
 	G::window.Create(sf::VideoMode(640, 480, 32), "GW");
-	while (1) {}
+	while (G::window.IsOpened()) {
+		G::window.Display();
+
+		handleEvents();
+	}
+}
+
+void handleEvents () {
+	sf::Event e;
+	while (G::window.GetEvent(e))
+		handleEvent(e);
+}
+
+void handleEvent(sf::Event e) {
+	switch(e.Type) {
+	case sf::Event::Closed:
+		G::window.Close();
+		break;
+	}
 }
